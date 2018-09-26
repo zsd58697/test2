89531687140af57146ff19482767176d90959654
root <root@localhost.localdomain>
2018-09-26 15:55:09 +0800

    add day02.py

--- day02.py
+++ day02.py
@@ -20,14 +20,120 @@ for i in "sddf@22":
 print(res)
 '''
 '''
+3.zi fu chuan xiang jia shu chu
 a = 'welcome'
 b = ' to'
 c = ' python'
 print(''.join((a,b,c)))
-'''
-name = eval (raw_input("enter a name:"))
+
+name = str(raw_input("enter a name:"))
 b= ' a handsome boy '
-print(''.join((name,b)))
+print(name+b)
+'''
+'''
+if 'a' < 'b':
+    print("ok")
+if 'a' < 'c':
+    print("ok1")
+'''
+'''
+3.
+ji ou shu
+a = eval(raw_input("enetr a number:"))
+if int(a%2) == 0:
+    print("ou")
+else:
+    print("ji")
+'''
+'''
+if yu ju
+A = str (raw_input("the boy yes or no  handsome:"))
+if A == 'yes':
+    print("next problem")
+    A == str(raw_input("the boy yes or no have mony"))
+    if A=='yes':
+        print("next problem")
+        A == str(raw_input("the boy yes or no have lao po:"))
+        if A=='no':
+            print("next problem")
+            A == str(raw_input("the boy yes or no have house:"))
+            if A=='yes':
+                print("i want to see see")
+            else:
+                print("i want to die")
+        else:
+            print("go out")
+    else:
+        print("go out")
+else:
+    print("go out")
+'''
+'''
+sui ji
+from numpy import *
+for i in range (10):
+    random.seed(10)
+    print(random.randint(0,10))
+'''
+'''
+4.cai shu
+import random
+A = eval(raw_input("enter a number between 0 and 10:"))
+if A == random.randint(1,10):
+    print("smart")
+elif A > random.randint(1,10):
+    print("so big")
+else:
+    print("so small")
+'''
+import random
+A = int(random.randint(0,9))
+B = int(random.randint(0,9))
+print("{} {}".format(A,B))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
