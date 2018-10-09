45d41ec6139a5881e4673d533b23bcf95f2e4288
root <root@localhost.localdomain>
2018-10-09 19:13:02 +0800

    zuoye

--- day05.py
+++ day05.py
@@ -24,8 +24,8 @@ A.winthdraw(2500)
 A.deposit(3000)
 A.getMonre()
 '''
-'''
-2.
+
+
 import math
 class regularPolygon:
     def __init__(self,n=3,side=1,x=0,y=0):
@@ -38,17 +38,16 @@ class regularPolygon:
         print("the duo bian xing perimeter is {}".format(self.P))
 
     def Area(self):
-        self.s=self.__side*self.__side
-        self.t=(math.sin(math.pi/self.__n))/(math.cos(math.pi/self.__n))
-        self.a=(self.__n*self.s)/4*self.t
+        self.s=pow(self.__side,2)
+        self.t=math.tan(math.pi/self.__n)
+        self.a=(self.__n*self.s)/(4*self.t)
         print("the duo bian xing area is {}".format(self.a))
 regularPolygon().getPerimeter()
 regularPolygon().Area()
-regularPolygon(x=6,y=4).getPerimeter()
-regularPolygon(x=6,y=4).Area()
+regularPolygon(6,4).getPerimeter()
+regularPolygon(6,4).Area()
 regularPolygon(10,4,5.6,7.8).getPerimeter()
 regularPolygon(10,4,5.6,7.8).Area()
-'''
 
 
 
