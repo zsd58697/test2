5403ad9c0ac4e0082280f5ae65eaeb33340b2c9f
root <root@localhost.localdomain>
2018-10-10 09:59:18 +0800

    zuoye

--- 1.py
+++ 1.py
@@ -8,26 +8,32 @@ class xd:
         self.y3=y3
         self.x4=x4
         self.y4=y4
+        self.k1=0
+        self.k2=0
+        self.b1=0
+        self.b2=0
+        self.x=0
+        self.y=0
     def k(self):
         self.k1=(self.y2-self.y1)/(self.x2-self.x1)
         self.k2=(self.y4-self.y3)/(self.x4-self.x3)
-        if k2==k1:
+        if self.k2==self.k1:
             print("no have jiao dian")
         else:
-            #print(k1,k2)
+            print(self.k1,self.k2)
     def b(self):
         self.b1=-(self.k1*self.x1-self.y1)
-        self.b2=-(sele.k2*self.x3-self.y3)
+        self.b2=-(self.k2*self.x3-self.y3)
     def Yzuobiao(self):
         self.y=(self.b1-(self.k1*self.b2))/(self.k2-self.k1)
-    def Xzuobiao(self)
+    def Xzuobiao(self):
         self.x=(self.y-self.b2)/self.k2
-        return x
-    def a(self)
-        print(x,y)
-xd(2,2,0,0,0,2,2,0).k()
-xd(2,2,0,0,0,2,2,0).b()
-xd(2,2,0,0,0,2,2,0).Yzuobiao()
-xd(2,2,0,0,0,2,2,0).Xzuobiao()
-xd(2,2,0,0,0,2,2,0).a()
+    def a(self):
+        print(self.x,self.y)
+A=xd(2,2,0,0,0,2,2,0)
+A.k()
+A.b()
+A.Yzuobiao()
+A.Xzuobiao()
+A.a()
 
--- day05.py
+++ day05.py
@@ -116,7 +116,7 @@ xd(2,2,0,0,0,2,2,0).Xzuobiao()
 xd(2,2,0,0,0,2,2,0).a()
 '''
 
-
+'''
 5.
 class LinearEquation:
     def __init__(self,a,b,c,d,e,f):
@@ -143,6 +143,46 @@ A=LinearEquation(3,4,2,3,1,1)
 A.isSolvable()
 A.getX()
 A.getY()
+'''
+
+class xd:
+    def __init__(self,x1,y1,x2,y2,x3,y3,x4,y4):
+        self.x1=x1
+        self.y1=y1
+        self.x2=x2
+        self.y2=y2
+        self.x3=x3
+        self.y3=y3
+        self.x4=x4
+        self.y4=y4
+        self.k1=0
+        self.k2=0
+        self.b1=0
+        self.b2=0
+        self.x=0
+        self.y=0
+    def k(self):
+        self.k1=(self.y2-self.y1)/(self.x2-self.x1)
+        self.k2=(self.y4-self.y3)/(self.x4-self.x3)
+        if self.k2==self.k1:
+            print("no have jiao dian")
+        else:
+            print(self.k1,self.k2)
+    def b(self):
+        self.b1=-(self.k1*self.x1-self.y1)
+        self.b2=-(self.k2*self.x3-self.y3)
+    def Yzuobiao(self):
+        self.y=(self.b1-(self.k1*self.b2))/(self.k2-self.k1)
+    def Xzuobiao(self):
+        self.x=(self.y-self.b2)/self.k2
+    def a(self):
+        print(self.x,self.y)
+A=xd(2,2,0,0,0,2,2,0)
+A.k()
+A.b()
+A.Yzuobiao()
+A.Xzuobiao()
+A.a()
 
 
 
