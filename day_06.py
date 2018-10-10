3f97eea7eb496f268056a91e4ed5895c0c4b9bb2
root <root@localhost.localdomain>
2018-10-10 21:07:33 +0800

    zuoye

---
+++ day_06.py
@@ -0,0 +1,192 @@
+# _*_coding:utf8-
+'''
+ 找男女开房的个数：
+file = open('/root/wangan.txt',encoding='utf-8',errors='ignore')
+s1=0
+s2=0
+for i in range(1000000):
+    line=file.readline()
+    if 'M' in line:
+        s2=s2+1
+    if 'F' in line:
+        s1=s1+1
+print("man have {},woman have {}".format(s2,s1))
+file.close()
+'''
+
+'''
+输出邮箱：
+file = open('/root/wangan.txt',encoding='utf-8',errors='ignore')
+for i in range(1000000):
+    line=file.readline()
+    if '@' in line:
+        print(line)
+file.close()
+'''
+
+
+'''
+判断社会安全号码是否符合规范：
+def pd(q):
+    global a
+    if len(a)==11:
+        if a[0]>='0' and a[0]<='9' and a[1]>='0' and a[1]<='9' and a[2]>='0' and a[2]<='9':
+            if a[3]=='-':
+                if (a[4]>='0' and a[4]<='9')and (a[5]>='0' and a[5]<='9'):
+                    if a[6]=='-':
+                        if (a[7]>='0' and a[7]<='9') and (a[8]>='0' and a[8]<='9') and (a[9]>='0' and a[9]<='9') and (a[10]>='0' and a[10]<='9'):
+                            print("Valid SSN")
+                        else:
+                            print("Invalid SSN")
+                    else:
+                        print("Invalid SSN")
+                else:
+                    print("Invalid SSN")
+            else:
+                print("Invalid SSN")
+        else:
+            print("Invalid SSN")
+    else:
+        print("Invalid SSN")
+
+a=str(raw_input("enter a string li ru ddd-dd-dddd:"))
+pd(a)
+'''
+'''
+2.求 子 串
+a=raw_input("enter a string:")
+b=raw_input("enter anthor string:")
+def find(c,d):
+    global a,b
+    for i in b:
+        if i not in a:
+            print("b no a zi chuan")
+            break
+    else:
+        print("b is a zi chuan")
+
+find(a,b)
+'''
+'''
+2.2
+a=raw_input("enter a string:")
+b=raw_input("enter anthor string:")
+if b in a:
+    print("b is a zi chuan")
+else:
+    print("b no a zi chuan")
+'''
+'''
+a=raw_input("enter password:")
+class ep3:
+    def __init__(self,a):
+        self.a=a
+    def pd(self):
+        if len(self.a)>=8:
+            return self.a
+        else:
+            print("invalid password ,chang du bu gou")
+    def pd2(self):
+        a=self.pd()
+        b = a.isalnum()
+        if( b==True):
+            return a
+        else:
+            print("invalid password ,no have zi mu or no have shu")
+    def pd3(self):
+        a=self.pd2()
+        s=0
+        for i in range(len(a)+1):
+            if a[i]>='0'and a[i]<='9':
+                s=s+1
+        if s>2:
+            print("true")
+        else:
+            print("invalid password")
+A=ep3(a)
+A.pd3()
+
+'''
+'''
+3.判断password
+def A(n):
+    global s,a
+    s=0
+    if len(a)>=8:
+        if (a.isalnum() == True):
+            for i in a:
+                if (i>='0' and i<='9'):
+                    s=s+1
+            if s>=2:
+                print("valid password")
+            else:
+                print("invalid password")
+        else:
+            print("invalid password")
+    else:
+        print("invalid password")
+a=raw_input("enter a password:")
+A(a)
+'''
+'''
+4.统计字母个数
+def countletters(s1):
+    global a,s
+    s=0
+    for i in a:
+        if i>='a' and i<='z':
+            s=s+1
+    print(s)
+a=raw_input("enter a string:")
+countletters(a)
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
