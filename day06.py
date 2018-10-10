01e73437d48a292148e2a69e9748cd6e46b9f5c7
root <root@localhost.localdomain>
2018-10-10 14:31:10 +0800

    shang ke

---
+++ day06.py
@@ -0,0 +1,21 @@
+'''
+a="http://www.baidu.com?page="
+b=" wd=xiaopangzi"
+for i in range(100):
+    print (a+str(i)+b)
+'''
+
+'''
+b=[1,2,3,'1',True]
+print b 
+c=list('123')
+print c
+'''
+'''
+a=[1,1,1,1,2,2,2,3]
+b=[]
+for i in (a):
+    if i not in b:
+        b.append(i)
+print b
+'''
